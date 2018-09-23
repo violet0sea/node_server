f7b0a98355cb516d54ad605b55e3b41bdc46287d
haohaiwang <dudulu@haohaiwangdeMBP.lan>
2018-09-23 10:27:46 +0800

    add fun

--- router/page.js
+++ router/page.js
@@ -15,8 +15,10 @@ class Page {
             let fullStaticPath = path.join(__dirname, '../', staticPath);
             console.log(fullStaticPath)
             // 获取静态资源内容，有可能是文件内容，目录，或404
-            var text = fs.readFileSync(path.join(fullStaticPath,'vue.html'), 'utf8');
-            ctx.response.body = text;
+            ctx.response.type = 'text/html';
+            ctx.response.body = fs.createReadStream(path.join(fullStaticPath,'vue.html'), 'utf8');
+            // fs.createReadStream(path.join(fullStaticPath,'vue.html'), 'utf8').pipe(ctx.res);
+            // ctx.response.body = text;
 
             // 设置请求头
             if(config.cacheControl) {
